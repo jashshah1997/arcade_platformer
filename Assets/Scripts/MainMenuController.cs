47faa914bfde58d821db002d8c41fb9a22682dbe
predator1245 <jashshah37@yahoo.com>
2021-09-25 16:29:02 +0200

    Add Initial Main Menu functionality

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Filename:            MainMenuController.cs
+ * Student Name:        Jash Shah
+ * Student ID:          101274212
+ * Date last modified:  25/09/2021
+ * Program Description: Controls Main Menu functions
+ * Revision History:
+ *  - 25/09/2021 - Add Initial Main Menu functions
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnInstructionsButton()
+    {
+        // TODO: Activate instructions
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
