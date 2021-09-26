4473a8a323806036cdfc9e6882b6052c9e4fd938
predator1245 <jashshah37@yahoo.com>
2021-09-26 10:39:57 +0200

    Add Instructions menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,10 +2,11 @@
  * Filename:            MainMenuController.cs
  * Student Name:        Jash Shah
  * Student ID:          101274212
- * Date last modified:  25/09/2021
+ * Date last modified:  26/09/2021
  * Program Description: Controls Main Menu functions
  * Revision History:
  *  - 25/09/2021 - Add Initial Main Menu functions
+ *  - 26/09/2021 - Add instruction screen functionality
  */
 
 using System.Collections;
@@ -15,18 +16,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    GameObject m_main_menu;
+    GameObject m_instruction_screen;
+
+    void Start()
+    {
+        m_main_menu = GameObject.FindGameObjectWithTag("MainMenu");
+        m_instruction_screen = GameObject.FindGameObjectWithTag("InstructionScreen");
+        
+        m_main_menu.SetActive(true);
+        m_instruction_screen.SetActive(false);
+    }
+
     public void OnStartButton()
     {
         SceneManager.LoadScene("GameplayScene");
     }
 
+    public void OnBackButton()
+    {
+        toggle_main_and_instruction_menu();
+    }
+
     public void OnInstructionsButton()
     {
-        // TODO: Activate instructions
+        toggle_main_and_instruction_menu();
     }
 
     public void OnExitButton()
     {
         Application.Quit();
+    }
+
+    void toggle_main_and_instruction_menu()
+    {
+        var is_active = m_main_menu.activeSelf;
+
+        m_main_menu.SetActive(!is_active);
+        m_instruction_screen.SetActive(is_active);
     }
 }
